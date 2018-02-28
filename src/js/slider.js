22ee44e75c5ff371c78fb9bd3a86a0202de85c42
PedashHelena <pedashhelena@gmail.com>
2018-02-28 20:57:08 +0200

    creativesolutions

---
+++ src/js/slider.js
@@ -0,0 +1,35 @@
+// slider.js
+;$(document).ready(function(){
+  $('#slider--works').slick({
+    lazyLoad: 'ondemand',
+    dots: true,
+    nextArrow: '<svg class="slick-next works__arrow"><use xlink:href="./img/svg-icons.svg#arrow-right" /></svg>',
+    prevArrow: '<svg class="slick-prev works__arrow"><use xlink:href="./img/svg-icons.svg#arrow-left" /></svg>',
+  });
+  $('#slider--teams').slick({
+    dots: false,
+    nextArrow: '<svg class="slick-next team__arrow"><use xlink:href="./img/svg-icons.svg#arrow-right" /></svg>',
+    prevArrow: '<svg class="slick-prev team__arrow"><use xlink:href="./img/svg-icons.svg#arrow-left" /></svg>',
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+  });
+});
+// slider.js END
